569bfe41f7d599faa0ab30077e17d227b4d4749c
DiSalvio <stephendisalvio@protonmail.com>
2022-03-15 11:55:48 +0800

    Remove some scaffold, import starting JSON data to home view

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{goals}}
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+export default {
+  data() {
+    return {
+      goals: sourceData.goals
+    }
+  }
+}
+</script>
