bbdc700783c8188689b7a58a6b78fdf57b1dffcc
DiSalvio <stephendisalvio@protonmail.com>
2022-03-16 10:58:54 +0800

    Add basic bootstrap styling to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
-    <div v-for="goal in goals" :key="goal.id">
-      <h1>{{goal.name}}</h1>
-      <p>{{goal.description}}</p>
+  <div class="container list-group">
+    <h1 class="text-muted">Goals</h1>
+    <div v-for="goal in goals" :key="goal.id" class="list-group-item">
+      <div class="list-group-item-heading d-flex justify-content-between align-items-center">
+        <h2>{{goal.name}}</h2>
+        <span v-if="goal.completed" class="badge badge-secondary badge-pill">Done</span>
+        <span v-else class="badge badge-warning badge-pill">In Progress</span>
+      </div>
+      <p class="list-group-item-light list-group-item-secondary">{{goal.description}}</p>
     </div>
   </div>
 </template>
