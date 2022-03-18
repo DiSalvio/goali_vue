4df31ea1db4ec43fc7a9e1b6e8391185e129e190
DiSalvio <stephendisalvio@protonmail.com>
2022-03-18 10:34:00 +0800

    Add router links to goal show pages from home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <h1 class="text-muted">Goals</h1>
     <div v-for="goal in goals" :key="goal.id" class="list-group-item">
       <div class="list-group-item-heading d-flex justify-content-between align-items-center">
-        <h2>{{goal.name}}</h2>
+        <h2><router-link :to="{name: 'PageGoalShow', params: {id: goal.id}}">{{goal.name}}</router-link></h2>
         <span v-if="goal.completed" class="badge badge-secondary badge-pill">Done</span>
         <span v-else class="badge badge-warning badge-pill">In Progress</span>
       </div>
