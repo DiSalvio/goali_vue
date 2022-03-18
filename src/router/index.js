bd5b508c30e42e164cd9eede7e238dc1c3586084
DiSalvio <stephendisalvio@protonmail.com>
2022-03-18 13:20:11 +0800

    Add soft 404 page and navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Home from '@/views/Home.vue'
 import PageGoalShow from '@/views/PageGoalShow.vue'
+import PageNotFound from '@/views/PageNotFound.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import sourceData from '@/data.json'
 
 const routes = [ 
   {
@@ -12,6 +14,25 @@ const routes = [
     path: '/goal/:id',
     name: 'PageGoalShow',
     component: PageGoalShow,
+    props: true,
+    beforeEnter (to, from, next){
+      const goalExists = sourceData.goals.find(goal => goal.id === parseInt(to.params.id))
+      if (goalExists) {
+        next()
+      } else {
+        next({
+          name: 'PageNotFound',
+          params: { resource: 'goal', pathMatch: to.path.substring(1).split('/') },
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'PageNotFound',
+    component: PageNotFound,
     props: true
   }
 ]
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-center">
+    <h1>The {{ resource || 'page' }} you're looking for doesn't exist</h1>
+    <h1><router-link :to="{name: 'Home'}">Home</router-link></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      required: false,
+      type: String
+    }
+  }
+}
+</script>
