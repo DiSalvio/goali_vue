3887abf6568b357342576c92ad78cfc6d8ba95dd
DiSalvio <stephendisalvio@protonmail.com>
2022-03-26 10:25:16 +0800

    put goal add component form on home page

---
+++ src/components/GoalAdd.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="list-group-item">
+    <form @submit.prevent="saveGoal">
+      <div class="form-group">
+        <label for="newName">Goal Name</label>
+        <input v-model="newName" type="text" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>Goal Description</label>
+        <input v-model="newDescription" type="text" class="form-control">
+      </div>
+      <div>
+        <button type="submit" class="btn btn-primary">Add Goal</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newName: "",
+      newDescription: ""
+    }
+  },
+  methods: {
+    saveGoal() {
+      const newGoal = {
+        name: this.newName,
+        description: this.newDescription
+      }
+
+      this.$emit('addGoal', { newGoal })
+
+      this.newName = ""
+      this.newDescription = ""
+    }
+  }
+}
+</script>
--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,18 +1,33 @@
 <template>
-  <GoalList :goals="goals"/>
+  <div class="container list-group">
+    <GoalList :goals="goals"/>
+    <GoalAdd @addGoal="saveGoal"/>
+  </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
 import GoalList from '@/components/GoalList.vue'
+import GoalAdd from '@/components/GoalAdd.vue'
 export default {
   components: {
-    GoalList
+    GoalList,
+    GoalAdd
   },
   data() {
     return {
       goals: sourceData.goals
     }
   },
+  methods: {
+    saveGoal (eventData) {
+      const goal = {
+        ...eventData.newGoal,
+        completed: false,
+        id: this.goals[this.goals.length - 1].id + 1
+      }
+      this.goals.push(goal)
+    }
+  }
 }
 </script>
