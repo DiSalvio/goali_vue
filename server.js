23394beebd5a0e80b86bfc3e652ef0b451cd9f10
DiSalvio <stephendisalvio@protonmail.com>
2022-04-15 14:23:54 +0800

    change server config

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors);
 app.use(express.static(path.join(__dirname, '/dist')))
 const port = process.env.PORT || 8000;
 app.get('/', (req, res) => {
-  res.sendFile(path.join('./', './dist/index.html'))
+  res.sendFile(path.join(__dirname, './dist/index.html'))
 })
  
 app.listen(port);
