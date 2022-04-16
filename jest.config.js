106d8b3d3acb39e9220983e07379dbeefce86cd6
DiSalvio <stephendisalvio@protonmail.com>
2022-04-16 16:53:58 +0800

    add vue jest basic config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest',
+  transform: {
+    '^.+\\.vue$': 'vue-jest'
+  },
+  moduleNameMapper: {
+    "/^@\/(.*)$/": "/Users/stephendisalvio/coding_projects/vue_projects/goali_vue/src/$1"
+  },
+  "resolver": null
+}
---
+++ tests/unit/pagehome.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils'
+import PageHome from '@/views/PageHome.vue'
+
+describe('PageHome.vue', () => {
+  it('has computed', () => {
+    /* const wrapper = shallowMount(PageHome, { */
+    /* }) */
+    /* expect(wrapper.text()).toMatch('') */
+  })
+})
