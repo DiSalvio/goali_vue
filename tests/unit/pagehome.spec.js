f765c8c997e8ca348a72a2f68052d3ea99d43499
DiSalvio <stephendisalvio@protonmail.com>
2022-05-16 17:59:47 +0800

    Add snapshot tests for homepage component html with and without goals

--- tests/unit/pagehome.spec.js
+++ tests/unit/pagehome.spec.js
@@ -1,10 +1,42 @@
-import { shallowMount } from '@vue/test-utils'
+import store from '@/store'
+import { mount } from '@vue/test-utils'
 import PageHome from '@/views/PageHome.vue'
 
 describe('PageHome.vue', () => {
-  it('has computed', () => {
-    /* const wrapper = shallowMount(PageHome, { */
-    /* }) */
-    /* expect(wrapper.text()).toMatch('') */
+  it('matches previous html snapshot if there are no goals', () => {
+    const wrapper = mount(PageHome, {
+      global: {
+        plugins: [store]
+      }
+    })
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('matches previous html snapshot if there are goals', async () => {
+    const wrapper = mount(PageHome, {
+      global: {
+        mocks: {
+          $store: {
+            dispatch: jest.fn(),
+            getters: {
+              'activeGoals': [ 
+                {
+                  "id":1,
+                  "name":"goal 1",
+                  "description":"description 1",
+                  "timestamp":"2022-04-15T06:58:03.382281Z",
+                  "completed":true,
+                  "updated":"2022-04-15T07:25:23.269770Z",
+                  "removed":false,
+                  "user":6
+                }
+              ]
+            }
+          }
+        }
+      }
+    })
+
+    expect(wrapper).toMatchSnapshot()
   })
 })
